0ce4e1f056d71b12be4e59f21cb439b742e14b76
Greg Stimson <stimsong@yahoo.com>
2018-02-04 21:43:30 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@
 def valid_move?(board, index)
   
   if index.between?(0, 8) && taken == "true"
-#    (board[index] == " " || board[index] == "" || board[index] == nil)
+    (board[index] == " " || board[index] == "" || board[index] == nil)
     return true
   else
     return false
